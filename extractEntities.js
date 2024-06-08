f344a233704eb19debe5f526c5ab5658d26ff10d
kai-np <kai@iob.inc>
2024-06-08 15:46:24 +0200

    Update: Filtering & combinations rework to meet spec

--- extractEntities.js
+++ extractEntities.js
@@ -1,30 +1,49 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
 const config = require("./config/config.json").development;
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    host: config.host,
-    dialect: config.dialect,
-  }
-);
-async function extractEntities(searchTerm) {
-  // Split the search term into words and filter out terms with fewer than 3 characters
-  const terms = searchTerm
-    .split(" ")
-    .map((term) => term.toLowerCase())
-    .filter((term) => term.length >= 3)
-    .map((term) => `%${term}%`);
-
-  // If no valid terms are found, return an empty array
-  if (terms.length === 0) {
-    return [];
+class EntityExtractor {
+  // Connect to DB using existing DB config file.
+  constructor() {
+    this.sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      {
+        host: config.host,
+        dialect: config.dialect,
+      }
+    );
   }
 
-  // Create the query using ILIKE for partial matching
-  const query = `
+  // Main function to execute extraction on given search
+  async extractEntities(searchTerm) {
+    const terms = this.splitAndFilterTerms(searchTerm);
+    if (terms.length === 0) return [];
+
+    const results = await this.fetchResults(terms);
+    const cleanTerms = terms.map((term) => term.replace(/%/g, ""));
+    const termsWithMultipleEntityTypes =
+      this.fetchTermsWithMultipleMatchingEntityTypes(cleanTerms, results);
+    const filteredEntityResults = this.filterResultsByType(results);
+    const filteredResults = this.removeDuplicates(
+      filteredEntityResults,
+      termsWithMultipleEntityTypes
+    );
+    return this.generateCombinations(filteredResults);
+  }
+
+  // Prepare search into keyword format for DB search usage
+  splitAndFilterTerms(searchTerm) {
+    return searchTerm
+      .split(" ")
+      .map((term) => term.toLowerCase())
+      .filter((term) => term.length >= 3)
+      .map((term) => `%${term}%`);
+  }
+
+  // Fetch entries from DB without loading/locally comparing all entries using ILIKE (SOC)
+  async fetchResults(terms) {
+    const query = `
       SELECT 'City' as type, id, name FROM "Cities" WHERE LOWER(name) ILIKE ANY (array[:terms])
       UNION
       SELECT 'Brand' as type, id, name FROM "Brands" WHERE LOWER(name) ILIKE ANY (array[:terms])
@@ -34,36 +53,180 @@ async function extractEntities(searchTerm) {
       SELECT 'Diet' as type, id, name FROM "Diets" WHERE LOWER(name) ILIKE ANY (array[:terms]);
     `;
 
-  const results = await sequelize.query(query, {
-    replacements: { terms },
-    type: Sequelize.QueryTypes.SELECT,
-  });
-
-  // Initialize an array to store combinations
-  const combinations = [];
-
-  // Generate combinations of entities
-  for (let i = 0; i < results.length; i++) {
-    for (let j = i + 1; j < results.length; j++) {
-      const entityA = results[i];
-      const entityB = results[j];
-      combinations.push({
-        [entityA.type.toLowerCase()]: { id: entityA.id, name: entityA.name },
-        [entityB.type.toLowerCase()]: { id: entityB.id, name: entityB.name },
-      });
-    }
+    return await this.sequelize.query(query, {
+      replacements: { terms },
+      type: Sequelize.QueryTypes.SELECT,
+    });
   }
 
-  return combinations;
+  // Find search keywords with mulltiple entity type matches
+  fetchTermsWithMultipleMatchingEntityTypes(terms, results) {
+    const termsWithMultipleMatchingEntityTypes = [];
+
+    for (const term of terms) {
+      const matchingEntitiesForTerm = results.filter((entity) =>
+        entity.name.toLowerCase().includes(term)
+      );
+      if (matchingEntitiesForTerm.length > 1) {
+        termsWithMultipleMatchingEntityTypes.push({
+          term,
+          matches: matchingEntitiesForTerm,
+        });
+      }
+    }
+
+    return termsWithMultipleMatchingEntityTypes;
+  }
+
+  // Filter entries by count per type
+  filterResultsByType(results) {
+    const types = ["City", "Dish_Type", "Brand", "Diet"];
+    const filteredResults = {
+      typesWithMultipleEntries: [],
+      typesWithSingleEntries: [],
+    };
+
+    types.forEach((type) => {
+      const typeEntries = {
+        type,
+        entries: results.filter((result) => result.type === type),
+      };
+      if (typeEntries.entries.length === 1) {
+        filteredResults.typesWithSingleEntries.push(typeEntries);
+      } else if (typeEntries.entries.length > 1) {
+        filteredResults.typesWithMultipleEntries.push(typeEntries);
+      }
+    });
+
+    return filteredResults;
+  }
+
+  // Remove duplicate entries after filtering terms with mulltiple entity types + entity types with mulltiple entries
+  removeDuplicates(dataObject, searchArray) {
+    const newDataObject = JSON.parse(JSON.stringify(dataObject));
+
+    searchArray.forEach((searchTerm) => {
+      searchTerm.matches.forEach((match) => {
+        newDataObject.typesWithMultipleEntries.forEach((typeEntry) => {
+          typeEntry.entries = typeEntry.entries.filter(
+            (entry) => !(entry.type === match.type && entry.id === match.id)
+          );
+        });
+
+        newDataObject.typesWithSingleEntries.forEach((typeEntry) => {
+          typeEntry.entries = typeEntry.entries.filter(
+            (entry) => !(entry.type === match.type && entry.id === match.id)
+          );
+        });
+      });
+    });
+
+    newDataObject.typesWithMultipleEntries =
+      newDataObject.typesWithMultipleEntries.filter(
+        (typeEntry) => typeEntry.entries.length > 0
+      );
+    newDataObject.typesWithSingleEntries =
+      newDataObject.typesWithSingleEntries.filter(
+        (typeEntry) => typeEntry.entries.length > 0
+      );
+
+    return {
+      searchTermsWithMultipleEntries: searchArray,
+      termsWithMultipleEntries: newDataObject.typesWithMultipleEntries,
+      termsWithSingleEntries: newDataObject.typesWithSingleEntries,
+    };
+  }
+
+  // Generate combinations with filtered data
+  generateCombinations(output) {
+    const {
+      searchTermsWithMultipleEntries,
+      termsWithMultipleEntries,
+      termsWithSingleEntries,
+    } = output;
+
+    const combinations = [];
+    const allSingleEntries = termsWithSingleEntries.flatMap(
+      (singleEntry) => singleEntry.entries
+    );
+
+    if (searchTermsWithMultipleEntries.length === 0) {
+      if (termsWithMultipleEntries.length === 0) {
+        combinations.push(this.createCombination(null, null, allSingleEntries));
+      } else {
+        termsWithMultipleEntries.forEach((multipleEntry) => {
+          multipleEntry.entries.forEach((multipleEntryItem) => {
+            combinations.push(
+              this.createCombination(null, multipleEntryItem, allSingleEntries)
+            );
+          });
+        });
+      }
+    } else {
+      searchTermsWithMultipleEntries.forEach((searchTerm) => {
+        searchTerm.matches.forEach((match) => {
+          if (termsWithMultipleEntries.length === 0) {
+            combinations.push(
+              this.createCombination(match, null, allSingleEntries)
+            );
+          } else {
+            termsWithMultipleEntries.forEach((multipleEntry) => {
+              multipleEntry.entries.forEach((multipleEntryItem) => {
+                combinations.push(
+                  this.createCombination(
+                    match,
+                    multipleEntryItem,
+                    allSingleEntries
+                  )
+                );
+              });
+            });
+          }
+        });
+      });
+    }
+
+    return combinations;
+  }
+
+  // Create a combination entry making sure rules are maintained
+  createCombination(match, multipleEntryItem, singleEntries) {
+    const combination = [];
+    if (match)
+      combination.push({
+        type: match.type,
+        id: match.id,
+        name: match.name.trim(),
+      });
+    if (multipleEntryItem)
+      combination.push({
+        type: multipleEntryItem.type,
+        id: multipleEntryItem.id,
+        name: multipleEntryItem.name.trim(),
+      });
+    if (singleEntries.length > 0) {
+      singleEntries.forEach((singleEntryItem) => {
+        combination.push({
+          type: singleEntryItem.type,
+          id: singleEntryItem.id,
+          name: singleEntryItem.name.trim(),
+        });
+      });
+    }
+    return combination;
+  }
 }
 
-// Run the function
-extractEntities("sushi in london")
-  .then((result) => {
+// Execute The entity extractor
+(async () => {
+  const extractor = new EntityExtractor();
+  try {
+    // Users search query input
+    const result = await extractor.extractEntities("sushi in london");
     console.log(result);
-    sequelize.close();
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error(err);
-    sequelize.close();
-  });
+  } finally {
+    extractor.sequelize.close();
+  }
+})();
--- readme.md
+++ readme.md
@@ -2,7 +2,8 @@
 
 # Comments
 
-Unfortunately I ran short of time during the process as had unexpected work to attend too, and was only able to start the task this morning. However, I have managed to complete the majority of the task, the only areas for improvement is adding filtering / checks when generating the combinations to ensure no unnesscary/redundent data is returned (currently various combinations are returned, which may be usefull in some cases but is something I was unable too improve due to the above mentioned time constraints).
+This solution implements all the rules described in the specification. It functions reliably and generates accurate results which could be used to fetch additional data.
+It is however worth noting that in production a better approach may be too use NLP (specifically Named Entity Recognition), to identify entity types and keyword matches, while it would be a more advanced undertaking this could allow for broader user inputs and a better overall user experience.
 
 # Setting up the DB
 
@@ -11,7 +12,7 @@ Unfortunately I ran short of time during the process as had unexpected work to a
 - Create the DB, with "node createDB"
 - Migrate the DB with "npx sequelize-cli db:migrate"
 - Seed DB with: "npx sequelize-cli db:seed:all"
-- Test DB tables correct and return rows with: "node testDB"
+- Test DB tables created correctly and return correct row counts with: "node testDB"
 
 # Running the script
 
